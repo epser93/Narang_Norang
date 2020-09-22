545ed30490413340bd55b3775b783aafcab9cff1
Ubuntu <ubuntu@ip-172-26-13-55.ap-northeast-2.compute.internal>
2020-09-22 08:32:29 +0000

    server static modify

--- backend/AI_PJT3/urls.py
+++ backend/AI_PJT3/urls.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+# 정적파일 관련
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('api/admin/', admin.site.urls),
@@ -10,4 +14,4 @@ urlpatterns = [
     path('api/books/', include('books.urls')),
     path('api/service_center/', include('service_center.urls')),
     path('api/voices/', include('voices.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
