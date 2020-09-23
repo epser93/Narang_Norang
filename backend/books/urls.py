86dbde664a720de4e0af34fd2a77512dc0cc5542
김유기 <daum7766@naver.com>
2020-09-23 09:59:25 +0900

    restapi doc update

--- backend/books/urls.py
+++ backend/books/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('fairytale/', views.FairytaleList.as_view()),
-    path('fairytale/<int:pk>', views.FairytaleDetail.as_view()),
+    path('fairytale/<int:pk>/', views.FairytaleDetail.as_view()),
     path('genre/', views.GenreList.as_view()),
 ]
