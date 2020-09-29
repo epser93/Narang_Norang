60f33c5ebc0ca6be6174cbadc7da103cd8d4fa26
강현영 <dalkang000@gmail.com>
2020-09-29 16:11:41 +0900

    Update MyBookShelves.vue UI

--- frontend/src/views/navigation/MyBookShelves.vue
+++ frontend/src/views/navigation/MyBookShelves.vue
@@ -1,6 +1,43 @@
 <template>
 <div>
-  
+  <!-- 읽고 있어요 -->
+  <div class="mt-4">
+    <h2 class="mb-4">읽고 있어요 <b-icon icon="book"></b-icon></h2>
+    <b-container fluid class="p-2">
+      <b-row>
+        <b-col>
+          <b-img thumbnail class="thumbnail" fluid src="https://picsum.photos/250/250/?image=54" alt="Image 1"></b-img>
+        </b-col>
+        <b-col>
+          <b-img thumbnail class="thumbnail" fluid src="https://picsum.photos/250/250/?image=58" alt="Image 2"></b-img>
+        </b-col>
+        <b-col>
+          <b-img thumbnail class="thumbnail" fluid src="https://picsum.photos/250/250/?image=59" alt="Image 3"></b-img>
+        </b-col>
+      </b-row>
+    </b-container>
+    <hr>
+  </div>
+
+<!-- 보고 싶어요 -->
+  <div style="margin-top: 6vmin;">
+    <h2 class="mb-4">보고 싶어요 <b-icon icon="bookmark-heart"></b-icon></h2>
+    <b-container fluid class="p-2">
+      <b-row>
+        <b-col>
+          <b-img thumbnail class="thumbnail" fluid src="https://picsum.photos/250/250/?image=54" alt="Image 1"></b-img>
+        </b-col>
+        <b-col>
+          <b-img thumbnail class="thumbnail" fluid src="https://picsum.photos/250/250/?image=58" alt="Image 2"></b-img>
+        </b-col>
+        <b-col>
+          <b-img thumbnail class="thumbnail" fluid src="https://picsum.photos/250/250/?image=59" alt="Image 3"></b-img>
+        </b-col>
+      </b-row>
+    </b-container>
+    <hr>
+  </div>
+
 </div>
 </template>
 
@@ -13,6 +50,15 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.thumbnail {
+  box-shadow: 4px 10px 5px #00000063;
+}
 
+hr {
+  margin-top: -2rem;
+  margin-bottom: 1rem;
+  border-top: 8vmin solid #b07112b0;
+  width: 170vmin;
+}
 </style>
