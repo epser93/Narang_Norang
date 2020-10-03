74329e9fc8e370959b89e9bae5fc538912547dec
Eunseok <eunsuk93@naver.com>
2020-10-03 15:52:04 +0900

    S03P23C206-76 S03P23C206-121 목소리 모델 삭제, 훈련목소리 카테고리 상세조회

--- backend/voices/serializers.py
+++ backend/voices/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Caption, VoiceCategory, VoiceModel
+from .models import Caption, VoiceCategory, VoiceModel, TrainVoice
 from accounts.serializers import UserSerializer
 
 class TrainCaptionSerializer(serializers.ModelSerializer):
@@ -11,6 +11,13 @@ class TrainVoiceCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = VoiceCategory
         fields = ['id', 'name']
+
+class TrainVoiceSerializer(serializers.ModelSerializer):
+    voice_category = TrainVoiceCategorySerializer()
+    caption = TrainCaptionSerializer()
+    class Meta:
+        model = TrainVoice
+        fields= ['id', 'voice_category', 'train_file', 'caption']
 
 class VoiceModelSerializer(serializers.ModelSerializer):
     class Meta:
